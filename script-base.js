e537df8ca311ab95c3bb6c25062824a0bf3c194b
nazandrey <nazandrbox@yandex.ru>
2017-07-20 15:52:19 +0300

    Delete converting name to lower case when creating files

--- script-base.js
+++ script-base.js
@@ -83,21 +83,21 @@ util.inherits(Generator, yeoman.generators.NamedBase);
 Generator.prototype.appTemplate = function (src, dest) {
   yeoman.generators.Base.prototype.template.apply(this, [
     src + this.scriptSuffix,
-    path.join(this.env.options.appPath, dest.toLowerCase()) + this.scriptSuffix
+    path.join(this.env.options.appPath, dest) + this.scriptSuffix
   ]);
 };
 
 Generator.prototype.testTemplate = function (src, dest) {
   yeoman.generators.Base.prototype.template.apply(this, [
     src + this.scriptSuffix,
-    path.join(this.env.options.testPath, dest.toLowerCase()) + this.scriptSuffix
+    path.join(this.env.options.testPath, dest) + this.scriptSuffix
   ]);
 };
 
 Generator.prototype.htmlTemplate = function (src, dest) {
   yeoman.generators.Base.prototype.template.apply(this, [
     src,
-    path.join(this.env.options.appPath, dest.toLowerCase())
+    path.join(this.env.options.appPath, dest)
   ]);
 };
 
@@ -109,7 +109,7 @@ Generator.prototype.addScriptToIndex = function (script) {
       file: fullPath,
       needle: '<!-- endbuild -->',
       splicable: [
-        '<script src="scripts/' + script.toLowerCase().replace(/\\/g, '/') + '.js"></script>'
+        '<script src="scripts/' + script.replace(/\\/g, '/') + '.js"></script>'
       ]
     });
   } catch (e) {
