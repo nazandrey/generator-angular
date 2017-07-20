c69f2882a9362d345ff237d14a4990ffa3ca6766
nazandrey <nazandrbox@yandex.ru>
2017-07-20 15:39:49 +0300

    Change controller template

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -9,9 +9,5 @@
  */
 angular.module('<%= scriptAppName %>')
   .controller('<%= classedName %>Ctrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
