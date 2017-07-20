6642814d55c63a864170bc3c191f0289c986ba02
nazandrey <nazandrbox@yandex.ru>
2017-07-20 15:40:26 +0300

    Change factory test template

--- templates/javascript/spec/service.js
+++ templates/javascript/spec/service.js
@@ -2,17 +2,14 @@
 
 describe('Service: <%= cameledName %>', function () {
 
-  // load the service's module
   beforeEach(module('<%= scriptAppName %>'));
 
-  // instantiate service
   var <%= cameledName %>;
   beforeEach(inject(function (_<%= cameledName %>_) {
     <%= cameledName %> = _<%= cameledName %>_;
   }));
 
   it('should do something', function () {
-    expect(!!<%= cameledName %>).toBe(true);
-  });
 
+  });
 });
